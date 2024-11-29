39c479ac84a58347f277e0b1ade862df84f63cdd
gopalpatil0318 <gopalpatilrcpit@gmail.com>
2024-11-29 23:33:57 +0530

    proble solve in fetcting tpc

--- src/components/tpo/TpcList.tsx
+++ src/components/tpo/TpcList.tsx
@@ -31,7 +31,7 @@ export function TpcList() {
 
   const fetchTpcs = async () => {
     try {
-      const response = await axios.get<{ success: boolean; data: Tpc[] }>('/api/tpo/get-tpcs')
+      const response = await axios.get('/api/tpo/get-tpcs', { headers: { 'Cache-Control': 'no-cache' } });
       setTpcs(response.data.data)
       console.log(response.data.data)
     } catch (error) {
